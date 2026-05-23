00370399d54e0157748b5ca365c86428794f7120
Hosein-tehrani <ho3einvictorho3ein@gmail.com>
2026-05-23 20:28:11 +0330

    optimize images with next Image tag

--- components/event-detail/event-logistics.js
+++ components/event-detail/event-logistics.js
@@ -1,21 +1,21 @@
-import AddressIcon from '../icons/address-icon';
-import DateIcon from '../icons/date-icon';
-import LogisticsItem from './logistics-item';
-import classes from './event-logistics.module.css';
+import AddressIcon from "../icons/address-icon";
+import DateIcon from "../icons/date-icon";
+import LogisticsItem from "./logistics-item";
+import classes from "./event-logistics.module.css";
+import Image from "next/image";
 
 function EventLogistics({ date, address, image, imageAlt }) {
-
-  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
-  const addressText = address.replace(', ', '\n');
+  const addressText = address.replace(", ", "\n");
 
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <Image src={`/${image}`} alt={imageAlt} width={400} height={400} />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
